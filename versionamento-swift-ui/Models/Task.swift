9efa28e269f2f83472830d5c473b50d6eb0964a2
andre-flores <decomflores@gmail.com>
2025-11-18 10:10:46 -0300

    setup data models

---
+++ versionamento-swift-ui/Models/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  versionamento-swift-ui
+//
+//  Created by aluno-01 on 18/11/25.
+//
+
+import Foundation
+
+struct Task {
+    var name: String
+    var details: String
+    var category: TaskCategory
+    var isCompleted: Bool
+}
---
+++ versionamento-swift-ui/Models/TaskCategory.swift
@@ -0,0 +1,34 @@
+//
+//  TaskCategory.swift
+//  versionamento-swift-ui
+//
+//  Created by aluno-01 on 18/11/25.
+//
+
+import Foundation
+
+enum TaskCategory: String {
+    case education = "Education"
+    case fitness = "Fitness"
+    case groceries = "Groceries"
+    case health = "Health"
+    case home = "Home"
+    case personal = "Personal"
+    case reading = "Reading"
+    case shopping = "Shopping"
+    case travel = "Travel"
+    
+    var imageName: String {
+        switch self {
+        case .education: return "graduationcap.fill"
+        case .fitness: return "dumbbell.fill"
+        case .groceries: return "fork.knife"
+        case .health: return "pills.fill"
+        case .home: return "house.fill"
+        case .personal: return "person.fill"
+        case .reading: return "book.fill"
+        case .shopping: return "cart.fill"
+        case .travel: return "airplane"
+        }
+    }
+}
