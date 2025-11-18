f300aca6e585bd39f102a88b991149a581cb0f1c
andre-flores <decomflores@gmail.com>
2025-11-18 09:23:41 -0300

    Initial Commit

---
+++ versionamento-swift-ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  versionamento-swift-ui
+//
+//  Created by aluno-01 on 18/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
