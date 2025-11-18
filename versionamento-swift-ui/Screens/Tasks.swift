c184a7551a2fe48c6c462bcf48e154aee38a47e3
andre-flores <decomflores@gmail.com>
2025-11-18 10:26:58 -0300

    task category enum

---
+++ versionamento-swift-ui/Screens/Tasks.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIView.swift
+//  versionamento-swift-ui
+//
+//  Created by aluno-01 on 18/11/25.
+//
+
+import SwiftUI
+
+struct Tasks: View {
+    
+    var tasks: [Task] = [
+        Task(name: "Almoço", details: "no RU", category: .fitness, isCompleted: false),
+        Task(name: "Janta", details: "no RU", category: .fitness, isCompleted: false),
+        Task(name: "Lanche", details: "no RU", category: .fitness, isCompleted: false),
+        Task(name: "Café da Manhã", details: "no RU", category: .fitness, isCompleted: false)
+    ]
+    
+    var body: some View {
+        
+        if tasks.isEmpty {
+            EmptyStateView()
+        } else {
+            List (tasks) { task in
+                Text(task.name)
+            }
+        }
+    }
+}
+
+
+#Preview {
+    Tasks()
+}
