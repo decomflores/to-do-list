55a83103e58d92d64439bb51dfe54e2de69761ed
andre-flores <decomflores@gmail.com>
2025-11-21 10:40:00 -0300

    task and description inputs

--- versionamento-swift-ui/Screens/AddTask.swift
+++ versionamento-swift-ui/Screens/AddTask.swift
@@ -8,8 +8,75 @@
 import SwiftUI
 
 struct AddTask: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State var name: String = ""
+    @State var details: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            
+            ScrollView {
+                
+                VStack(spacing: 20) {
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Task")
+                            .font(.system(.subheadline, weight: .semibold))
+                            .padding(.horizontal, 8)
+                        TextField("Your task name here", text: $name)
+                            .padding(.vertical, 12)
+                            .padding(.horizontal)
+                            .background(
+                                RoundedRectangle(cornerRadius: 26)
+                                    .foregroundStyle(.backgroundTertiary)
+                            )
+                    }
+                    
+//                    input categoria
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Details")
+                            .font(.system(.subheadline, weight: .semibold))
+                            .padding(.horizontal, 8)
+                        TextField("More details about the task", text: $details, axis: .vertical)
+                            .lineLimit(5...10)
+                            .padding(.vertical, 12)
+                            .padding(.horizontal)
+                            .background(
+                                RoundedRectangle(cornerRadius: 26)
+                                    .foregroundStyle(.backgroundTertiary)
+                            )
+                    }
+                }
+                
+            }
+            
+            
+            .padding(.top)
+            .padding(.horizontal)
+            .background(.backgroundSecondary)
+            .navigationTitle(Text("Add Task"))
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                
+                
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel", systemImage: "xmark") {
+                        dismiss()
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add", systemImage: "paperplane") {
+                        //                            to do
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
+            
+        }
     }
 }
 
